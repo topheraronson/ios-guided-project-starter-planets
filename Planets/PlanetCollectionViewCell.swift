6723cc1557122d61a2bab05adc6b4f64a9e7cc0b
Christopher Aronson <christopheraronson@icloud.com>
2019-05-02 10:13:57 -0700

    Created PlanetsCollectionViewController and PlanetsCollectionViewCell Classes/Files

---
+++ Planets/PlanetCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Christopher Aronson on 5/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Planets/PlanetsCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PlanetsCollectionViewController.swift
+//  Planets
+//
+//  Created by Christopher Aronson on 5/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetsCollectionViewController: UICollectionViewController {
+    
+}
